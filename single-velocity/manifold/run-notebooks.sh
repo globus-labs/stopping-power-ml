375c7447bb9fbe0b70c9c1278494b812bd0a1af7
Logan Ward <ward.logan.t@gmail.com>
2018-05-17 13:34:56 -0400

    Added initial attempt to get "optimized" trajectories

---
+++ single-velocity/manifold/run-notebooks.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+# This script just runs through all of the current notebooks in the proper order
+
+runJupyter() {
+    jupyter nbconvert --execute --inplace --ExecutePreprocessor.timeout=-1 $1
+}
+
+# If we get too many files, we should probably create a file containing
+#  names of notebooks and the order in which they should be executed
+for n in `seq 0 1`; do
+    runJupyter ${n}_*nb
+done
