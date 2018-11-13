8f01b6a0b956570ebc8dabf0ae8440af6d2dba63
Logan Ward <WardLT@users.noreply.github.com>
2018-11-13 14:14:18 -0600

    Update run-notebooks.sh

--- run-notebooks.sh
+++ run-notebooks.sh
@@ -8,7 +8,7 @@ runJupyter() {
 
 # If we get too many files, we should probably create a file containing
 #  names of notebooks and the order in which they should be executed
-for file in `seq 0 1`; do
+for file in `seq 0 2`; do
     runJupyter ${file}_*nb
 done
 
